9d649089a20ff3135a27c92904a58bc3b5b08036
Vishal Jadhav <vishal.jadhav@pocketapp.co.uk>
2020-09-21 01:06:51 +0530

    feat: charts

--- app/screens/Stats/index.js
+++ app/screens/Stats/index.js
@@ -1,16 +1,75 @@
 import React, { useEffect } from 'react';
 import {
-  View, Text
+  View, Text, Dimensions
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { PieChart } from 'react-native-chart-kit';
+import GuestData from '../../assets/mock_data.json';
 
 import style from '../../assets/styles/theme';
 
 const Stats = (props) => {
+  const data = [
+    {
+      name: "Seoul",
+      population: 21500000,
+      color: "rgba(131, 167, 234, 1)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Toronto",
+      population: 2800000,
+      color: "#F00",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Beijing",
+      population: 527612,
+      color: "red",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "New York",
+      population: 8538000,
+      color: "#ffffff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    },
+    {
+      name: "Moscow",
+      population: 11920000,
+      color: "rgb(0, 0, 255)",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15
+    }
+  ];
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
   return (
     <SafeAreaView style={style.sf_wpr}>
       <View style={style.cnt_wpr}>
-        <Text>Stats</Text>
+        <PieChart
+          data={data}
+          width={Dimensions.get('screen').width}
+          height={220}
+          chartConfig={chartConfig}
+          accessor="population"
+          backgroundColor="transparent"
+          paddingLeft="15"
+          absolute
+        />
       </View>
     </SafeAreaView>
   );
